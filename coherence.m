2beb634cf4ade8a12e697451cd163c7844cda2e6
Mariella <mariella@linux-3nn0.epfl.ch>
2017-05-05 15:53:27 +0200

    Copy and pasted my files and coherence

--- coherence.m
+++ coherence.m
@@ -1,13 +1,23 @@
-function mu = coherence(A)
-%COHERENCE Coherence of a matrix A
-%
-%   mu = coherence(A) returns the coherence of matrix A.
-%   The columns of A are first orthonormalized.
-%
-A = orth(A);
+% Script regarding the concept of coherence. Average coherence of random
+% matrices
+close all;
+clear all;
+n=1000; % number of matrices
 
-row_norms = sqrt(sum(abs(A).^2,2));
-mu = max(row_norms);
+avg_coherence=0;
 
-
+for i=1:1000
+    A = rand(1000,50);
+    [Q, R] = qr(A,0);
+    cohr(i) = max(sum (Q .^2,2));
+    avg_coherence = avg_coherence +cohr(i);
 end
+
+avg_coherence= avg_coherence/n;
+
+
+%% PLOTS
+set(0,'DefaultAxesFontSize',12)
+close all;
+hist(cohr)
+xlabel('Coherence \mu(A)')
